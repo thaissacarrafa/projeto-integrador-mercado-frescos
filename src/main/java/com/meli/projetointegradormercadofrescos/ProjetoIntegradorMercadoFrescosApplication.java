60cbb86fcd7592fd3b02d8b052b1d7af2041fb4d
thaissacarrafa <thaissacarrafa@gmail.com>
2022-11-08 11:16:51 -0300

    Startup

---
+++ src/main/java/com/meli/projetointegradormercadofrescos/ProjetoIntegradorMercadoFrescosApplication.java
@@ -0,0 +1,13 @@
+package com.meli.projetointegradormercadofrescos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorMercadoFrescosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorMercadoFrescosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/projetointegradormercadofrescos/ProjetoIntegradorMercadoFrescosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.projetointegradormercadofrescos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorMercadoFrescosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
