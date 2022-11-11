6efef71b1bee48e5ba84a4e9e6991f9d128f8ca1
thaissacarrafa <thaissacarrafa@gmail.com>
2022-11-11 14:40:07 -0300

    complet alterations

--- src/main/java/com/meli/projetointegradormelifrescos/enums/Category.java
+++ src/main/java/com/meli/projetointegradormelifrescos/enums/Category.java
@@ -1,18 +1,49 @@
 package com.meli.projetointegradormelifrescos.enums;
 
-public enum Category {
-    FRESCO("FS"),
-    REFRIGERADO("RF"),
-    CONGELADO("FF");
+import com.meli.desafio_quality.exception.NotFoundException;
+import com.meli.projetointegradormelifrescos.config.exception.BadRequestException;
+import lombok.Getter;
 
+public enum Category {
+    FRESCO("FS", "Fresco", 15F, 25F),
+    REFRIGERADO("RF", "Refrigerado", 8F, 15F),
+    CONGELADO("FF", "Congelado", 0F, 7F);
+
+    @Getter
     private String value;
 
-    Category(String value) {
+    @Getter
+    private String name;
+
+    @Getter
+    private Float minimumTemperature;
+
+    @Getter
+    private Float maximumTemperature;
+
+
+    Category(String value, String name, Float minimumTemperature, Float maximumTemperature) {
         this.value = value;
+        this.name = name;
+        this.minimumTemperature = minimumTemperature;
+        this.maximumTemperature = maximumTemperature;
     }
 
-    public String getCategory() {
-        return value;
+    public static Category valueOf(int categoryId) {
+        if (categoryId > 2 || categoryId < 0) {
+            throw new BadRequestException("invalid category");
+        }
+        return Category.values()[categoryId];
     }
 
+    public static Category getEnumName(String name) {
+        try {
+            return Category.valueOf(name.toUpperCase());
+        } catch (Exception e) {
+            throw new BadRequestException("Invalid category");
+        }
+
+    }
 }
+
+
--- src/main/java/com/meli/projetointegradormelifrescos/mapper/IBatchStockMapper.java
+++ src/main/java/com/meli/projetointegradormelifrescos/mapper/IBatchStockMapper.java
@@ -1,2 +1,13 @@
-package com.meli.projetointegradormelifrescos.mapper;public class Mapper {
+package com.meli.projetointegradormelifrescos.mapper;
+
+import com.meli.projetointegradormelifrescos.dto.BatchStockDTO;
+import com.meli.projetointegradormelifrescos.model.BatchStock;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface IBatchStockMapper {
+    IBatchStockMapper MAPPER = Mappers.getMapper(IBatchStockMapper.class);
+
+    BatchStock mappingBatchStockDTOToBatchStock(BatchStockDTO dto);
 }
