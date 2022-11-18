3ccfe0568f36fb2e7241d16149b4e593d62f6ee6
gugadolzan <dolzan.gustavo@gmail.com>
2022-11-18 00:21:33 -0300

    refactor: enum Category Getter

--- src/main/java/com/meli/projetointegradormelifrescos/enums/Category.java
+++ src/main/java/com/meli/projetointegradormelifrescos/enums/Category.java
@@ -4,22 +4,19 @@ import com.meli.projetointegradormelifrescos.exception.BadRequestException;
 
 import lombok.Getter;
 
+@Getter
 public enum Category {
     FRESCO("FS", "Fresco", 15F, 25F),
     REFRIGERADO("RF", "Refrigerado", 8F, 15F),
     CONGELADO("FF", "Congelado", 0F, 7F);
 
 
-    @Getter
     private String value;
 
-    @Getter
     private String name;
 
-    @Getter
     private Float minimumTemperature;
 
-    @Getter
     private Float maximumTemperature;
 
 
