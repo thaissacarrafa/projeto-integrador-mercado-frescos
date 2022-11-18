5b3284ebac3c6d063b48f85827c77bf1a455c458
gugadolzan <dolzan.gustavo@gmail.com>
2022-11-18 00:28:10 -0300

    refactor: announcement service
    Implements interface and overrides methods.

--- src/main/java/com/meli/projetointegradormelifrescos/service/AnnouncementService.java
+++ src/main/java/com/meli/projetointegradormelifrescos/service/AnnouncementService.java
@@ -1,46 +1,74 @@
 package com.meli.projetointegradormelifrescos.service;
 
+import com.meli.projetointegradormelifrescos.enums.Category;
 import com.meli.projetointegradormelifrescos.exception.NotFoundException;
 import com.meli.projetointegradormelifrescos.model.Announcement;
 import com.meli.projetointegradormelifrescos.repository.AnnouncementRepo;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 @Service
-public class AnnouncementService {
-        @Autowired
-        private AnnouncementRepo productRepository;
+public class AnnouncementService implements IAnnouncementService {
 
-    /***
-     *   @param message  método responsável por consultar todos os produtos
-      */
-        public List<Announcement> listAllProducts() {
-            List<Announcement> productList = productRepository.findAll();
-            if(productList.isEmpty()) {
-                throw new NotFoundException("Product list is empty");
-            }
-            return productList;
-        }
+    @Autowired
+    private AnnouncementRepo announcementRepo;
 
-    /***
-     *
-     * @param message     método responsável por consultar todos os produtos
-     * @return
+    /**
+     * Get all announcements
+     * @return List of announcements
      */
-    public List<Announcement> listByCategory(String category){
+    @Override
+    public List<Announcement> getAllAnnouncements() {
+        List<Announcement> announcements = announcementRepo.findAll();
 
-        List<Announcement> products = productRepository.findAll().stream()
-        .filter(product -> Objects.equals(product.getCategory(),category.toUpperCase()))
-
-                .collect(Collectors.toList());
-
-        if(products.isEmpty()){
-            throw new NotFoundException("No products found");
+        if (announcements.isEmpty()) {
+            throw new NotFoundException("No announcements found");
         }
-        return products;
+
+        return announcements;
+    }
+
+    /**
+     * Get all announcements by id
+     * @param id Announcement id
+     * @return List of a single announcement
+     */
+    @Override
+    public List<Announcement> getAnnouncementById(Long id) {
+        Announcement announcement = announcementRepo
+            .findById(id)
+            .orElseThrow(() -> new NotFoundException("Announcement not found"));
+
+        return List.of(announcement);
+    }
+
+    /**
+     * Get all announcements by category
+     * @param category Category to filter
+     * @return List of announcements
+     */
+    @Override
+    public List<Announcement> getAllAnnouncementsByCategory(String category) {
+        List<Announcement> announcements = announcementRepo.findByCategory(
+            Category.valueOf(category.toUpperCase())
+        );
+
+        if (announcements.isEmpty()) {
+            throw new NotFoundException(
+                "No announcements found for category " + category
+            );
+        }
+
+        return announcements;
+    }
+
+    // TODO: Feature 3
+    @Override
+    public List<Announcement> getAllAnnouncementsByBatchNumber(
+        Long id,
+        Character sortBy
+    ) {
+        return null;
     }
 }
