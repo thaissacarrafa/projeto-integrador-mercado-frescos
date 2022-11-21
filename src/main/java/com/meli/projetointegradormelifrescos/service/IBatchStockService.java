15e806921e5d778a78aaeef06cfd9fe927e3fa51
gugadolzan <dolzan.gustavo@gmail.com>
2022-11-21 14:54:50 -0300

    feat: IBatchStockService
    getBatchStockBySection and getBatchStockByCategory.

--- src/main/java/com/meli/projetointegradormelifrescos/service/IBatchStockService.java
+++ src/main/java/com/meli/projetointegradormelifrescos/service/IBatchStockService.java
@@ -1,9 +1,11 @@
 package com.meli.projetointegradormelifrescos.service;
 
-import com.meli.projetointegradormelifrescos.model.BatchStock;
+import com.meli.projetointegradormelifrescos.dto.BatchStockResDTO;
 
 import java.util.List;
 
 public interface IBatchStockService {
-        List<Batch> saveAll(List<Batch> batches);
+//        List<BatchStock> saveAll(List<BatchStock> batches);
+        BatchStockResDTO getBatchStockBySection(Integer numberOfDays, Long section);
+        BatchStockResDTO getBatchStockByCategory(Integer numberOfDays, String category, String sortBy);
 }
