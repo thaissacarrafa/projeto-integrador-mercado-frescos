4db5db76a54e7d8fa3030bbe0986d5237074377e
Igor S. Fernandes <igor.fernandes@mercadolivre.com>
2022-11-22 13:14:31 -0300

    feat: cria uma exeption para validação de credencial

---
+++ src/main/java/com/meli/projetointegradormelifrescos/exception/BadCredentialsException.java
@@ -0,0 +1,7 @@
+package com.meli.projetointegradormelifrescos.exception;
+
+public class BadCredentialsException extends RuntimeException{
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+}
